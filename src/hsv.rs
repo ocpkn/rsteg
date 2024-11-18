74ce151fdfb482a17f26c35096a252f3f1daf355
Oscar Popkin <opopkin@terpmail.umd.edu>
2024-11-18 16:56:49 -0500

    Add image normalization

---
+++ src/hsv.rs
@@ -0,0 +1,58 @@
+#[derive(Debug, Clone)]
+pub struct HSVColor {
+    pub hue: f32,
+    pub sat: f32,
+    pub val: f32,
+}
+
+impl HSVColor {
+    pub fn from_rgb<T: Into<f32>>(red: T, green: T, blue: T, depth: u8) -> Self {
+        let n: f32 = ((1 << depth as u8) - 1) as f32;
+        let (r, g, b) = (red.into() / n, green.into() / n, blue.into() / n);
+
+        let v: f32 = r.max(g.max(b));
+        let c: f32 = v - r.min(g.min(b));
+        
+        let h: f32 =
+            (if c == 0.0 {
+                0.0
+            } else if v == r {
+                ((g - b) / c) % 6.0
+            } else if v == g {
+                ((b - r) / c) + 2.0
+            } else if v == b {
+                ((r - g) / c) + 4.0
+            } else {
+                unreachable!();
+            }) * 60.0;
+
+        let s: f32 =
+            if v == 0.0 { 0.0 } else { c / v };
+
+        HSVColor { hue: h, sat: s, val: v }
+    }
+
+    pub fn to_rgb(&self, depth: u8) -> [u8; 3] {
+        let c = self.val * self.sat;
+        let h = self.hue / 60.0;
+        let x = c * (1.0 - (h % 2.0 - 1.0).abs());
+        let m = self.val - c;
+
+        let (r1, g1, b1): (f32, f32, f32) = match h {
+            v if v < 1.0 => (c, x, 0.0),
+            v if v < 2.0 => (x, c, 0.0),
+            v if v < 3.0 => (0.0, c, x),
+            v if v < 4.0 => (0.0, x, c),
+            v if v < 5.0 => (x, 0.0, c),
+            v if v < 6.0 => (c, 0.0, x),
+            _ => unreachable!()
+        };
+
+        let n = ((1 << depth as u8) - 1) as f32;
+        let r = ((r1 + m) * n) as u8;
+        let g = ((g1 + m) * n) as u8;
+        let b = ((b1 + m) * n) as u8;
+
+        [r, g, b]
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::fs::File;
 use std::io::BufWriter;
 use std::path::PathBuf;
 
+use rsteg::hsv::HSVColor;
+
 use clap::Parser;
 
 use rand::Rng;
@@ -34,16 +36,6 @@ struct Args {
         requires("conceal"))]
     image: Option<PathBuf>,
 
-    #[arg(short, long,
-        group="mode",
-        requires("key"))]
-    encrypt: bool,
-
-    #[arg(short, long,
-        group="mode",
-        requires("key"))]
-    decrypt: bool,
-
     #[arg(short, long, value_name="KEY",
         requires("mode"))]
     key: Option<u64>,
@@ -51,6 +43,14 @@ struct Args {
     #[arg(short, long, value_name="1-8", value_parser=clap::value_parser!(u8).range(1..9),
         default_value("8"))]
     bits: u8,
+
+    #[arg(short, long,
+        conflicts_with_all(["equalize", "reveal"]))]
+    stretch: bool,
+
+    #[arg(short, long,
+        conflicts_with_all(["reveal"]))]
+    equalize: bool,
 }
 
 fn decode_image(path: PathBuf, normalize: bool) -> (png::OutputInfo, Vec<u8>) {
@@ -88,17 +88,54 @@ fn main() {
         )
     ).collect();
 
+    let samples = samples - alpha;
+
+    if args.stretch {
+        let maxx: u8 = u8::MAX >> (8 - args.bits);
+
+        let minmaxs = input.chunks_exact(samples).fold(vec![(maxx, 0u8); samples],
+            |minmaxs, x| {
+                let b = x.iter().zip(minmaxs);
+
+                b.map(|(x, (min, max))| (min.min(*x), max.max(*x))).collect()
+            }
+        );
+
+        for x in input.chunks_exact_mut(samples) {
+            for (y, (min, max)) in x.iter_mut().zip(minmaxs.iter()) {
+                let new = if *max == 0 {0} else {(*y - *min) as u16 * maxx as u16 / (*max - *min) as u16};
+                *y = new as u8;
+            }
+        }
+    } else if args.equalize {
+        let hsvinput: Vec<HSVColor> = input.chunks_exact(samples).map(|px| {
+            let (r, g, b) = (px[0], px[1], px[2]);
+            HSVColor::from_rgb(r, g, b, args.bits)
+        }).collect();
+
+        let mut vals: Vec<f32> = hsvinput.iter().map(|hsv| hsv.val).collect();
+        vals.sort_by(|a, b| a.partial_cmp(&b).unwrap());
+        vals.dedup();
+
+        let cdf = |v| vals.binary_search_by(|a| a.partial_cmp(&v).unwrap()).unwrap() as f32 / (vals.len() - 1) as f32;
+
+        input = hsvinput.iter().flat_map(|hsv| {
+            HSVColor {
+                hue: hsv.hue,
+                sat: hsv.sat,
+                val: cdf(hsv.val)
+            }.to_rgb(args.bits)
+        }).collect();
+    }
+
     // Encrypt/Decrypt
     if let Some(key) = args.key {
         let mut rng = ChaCha20Rng::seed_from_u64(key);
 
-        let max: u16 = 1 << (args.bits);
+        let max: u8 = u8::MAX >> (8 - args.bits);
 
         for x in input.iter_mut() {
-            let rand = rng.gen_range(0..max);
-            let num = if args.decrypt || args.reveal {max - rand} else {rand};
-            let y = *x as u16 + num;
-            *x = (y % max) as u8;
+            *x ^= rng.gen_range(0..=max);
         }
     }
 
@@ -117,13 +154,13 @@ fn main() {
         let i_alpha: usize = (i_info.color_type as usize & 4) >> 2;
         let i_samples: usize = i_info.color_type.samples();
 
-        assert!(i_samples - i_alpha == samples - alpha);
+        assert!(i_samples - i_alpha == samples);
 
-        let px_iter = input.chunks_exact(samples - alpha);
+        let px_iter = input.chunks_exact(samples);
         let i_px_iter = i_buf.chunks_exact(i_samples);
 
         i_px_iter.zip(px_iter).flat_map(|(i_px, px)| {
-            (0..i_samples - i_alpha).map(|i| {
+            (0..samples).map(|i| {
                 (i_px[i] & mask) | px[i]
             })
         }).collect()
@@ -139,7 +176,7 @@ fn main() {
     };
 
     let file = File::create(args.output).unwrap();
-    let ref mut w = BufWriter::new(file);
+    let w = &mut BufWriter::new(file);
 
     let mut encoder = png::Encoder::new(w, info.width, info.height);
     encoder.set_color(png::ColorType::Rgb);
